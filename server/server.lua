6a52e29d9fda58ee2e10724d59462cc17700cef0
ohscxr <scarishawt@gmail.com>
2024-11-19 06:18:15 -0500

    fix(server) mysql syntax error fix

--- server/server.lua
+++ server/server.lua
@@ -1,7 +1,14 @@
 CreateThread(function()
     for k,v in pairs(Items) do
-        MySQL.insert('INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`, `weight`) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE label = VALUES(label), limit = VALUES(limit), can_remove = VALUES(can_remove), type = VALUES(type), usable = VALUES(usable), desc = VALUES(desc), weight = VALUES(weight)', {
-            k, v.label, v.limit, v.can_remove, v.type, v.usable, v.desc, v.weight
+        MySQL.insert('INSERT INTO `items` (`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`, `weight`) VALUES (:item, :label, :limit, :can_remove, :type, :usable, :desc, :weight) ON DUPLICATE KEY UPDATE label = :label, limit = :limit, can_remove = :can_remove, type = :type, usable = :usable, desc = :desc, weight = :weight', {
+            item = k,
+            label = v.label,
+            limit = v.limit,
+            type = v.type,
+            can_remove = v.can_remove,
+            usable = v.usable,
+            desc = v.desc,
+            weight = v.weight,
         })
     end
 end)
